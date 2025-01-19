8169adc98eadf75269b44eec511d0e3e0af6efb4
Kitty9079 <kantinant2011@gmail.com>
2025-01-19 22:38:33 +0700

    add more grind

--- Grind 75/26_Insert_interval.py
+++ Grind 75/26_Insert_interval.py
@@ -15,3 +15,23 @@ class Solution:
                 newInterval = [min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
         res.append(newInterval)
         return res
+
+
+
+#others
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        new_I = newInterval
+        I = intervals
+        res = []
+        
+        for i in range(len(I)):
+            if new_I[1] < I[i][0]:
+                res.append(new_I)
+                return res + I[i:]
+            elif new_I[0] > I[i][1]:
+                res.append(I[i])
+            else:
+                 new_I = [min(new_I[0],I[i][0]),max(new_I[1],I[i][1])]
+        res.append(new_I)
+        return res 
